30a2482a37c0aaac3f1a3e3520bc68ae06f84682
Oxie S <my_email@mail.com>
2026-02-05 00:54:48 -0500

    add click test

--- tests/simple/example.test.ts
+++ tests/simple/example.test.ts
@@ -4,5 +4,15 @@ test("Simple test", async ({ page }) => {
   await page.goto("https://www.example.com");
   const pageTitle = page.locator("h1");
   await expect(pageTitle).toContainText("Example Domain");
-  await page.waitForTimeout(3000);
+  await page.waitForTimeout(1000);
+});
+
+test("Sign In Test", async ({ page }) => {
+  await page.goto("http://zero.webappsecurity.com/index.html");
+  await page.click("#signin_button");
+  await page.waitForTimeout(2000);
+  await page.click("text=Sign in");
+  await page.waitForTimeout(2000);
+  const errorMsg = page.locator(".alert-error");
+  await expect(errorMsg).toContainText("Login and/or password are wrong.");
 });
