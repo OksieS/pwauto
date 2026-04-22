8702df3fd7ee67a68954c4d8312f39f38b6e6ad8
Oxie S <my_email@mail.com>
2026-04-22 00:02:50 -0400

    fix: change the name of reporter

--- e2e.config.ts
+++ e2e.config.ts
@@ -2,7 +2,7 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   timeout: 30000,
-  reporter: [["html"], ["gitHub"]],
+  reporter: [["html"], ["github"]],
   retries: 0,
   testDir: "tests/e2e",
   use: {
