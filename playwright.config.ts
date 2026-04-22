7c138e25a5ad850902667c984a17b8f0fe2e7a2f
Oxie S <my_email@mail.com>
2026-04-21 23:40:19 -0400

    Remove github reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   timeout: 30000,
-  reporter: [["html"], ["github"]],
+  reporter: "html",
   retries: 0,
   use: {
     headless: true,
